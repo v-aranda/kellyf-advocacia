11e578e16b554be3fcde52155d87b926316eef6e
V-aranda <124964947+V-aranda@users.noreply.github.com>
2023-02-10 01:27:04 -0300

    Add files via upload

---
+++ assets/scripts/script.js
@@ -0,0 +1,43 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.backgroundColor = "var(--navbg)";
+  } else {
+    document.getElementById("navbar").style.backgroundColor = "rgba(0,0,0,0)";
+   
+  }
+}
+
+
+  const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },    
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  })
+
+  const swiper2 = new Swiper(".swiper2", {
+    // Optional parameters
+    loop: true,
+    effect: "flip",
+    grabCursor: false,
+
+    autoplay: {
+      delay: 3500,
+    },
+
+    })
