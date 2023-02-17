d16c5d99b4acc37cd6067ead81a7560c523a5958
Varanda <124964947+V-aranda@users.noreply.github.com>
2023-02-17 02:53:58 -0300

    Update script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -41,3 +41,13 @@ function scrollFunction() {
     },
 
     })
+  
+  function initMap() {} // now it IS a function and it is in global
+
+    $(() => {
+      initMap = function() {
+        // your code like...
+        var map = new google.maps.Map(document.getElementById('map'), {/*your code*/});
+        // and other stuff...
+      }
+    })
